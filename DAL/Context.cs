a9170b9b366ef57f77cff55ee7bd2d75890b8154
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2023-09-29 16:09:17 -0400

    Continuando agregando los modelos y agregando el contexto

---
+++ DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ticket.Models;
+
+namespace Ticket.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> Opcions) : base(Opcions) { }
+        public DbSet<Prioridades> Prioridades { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Tickets> Tickets { get; set; }
+    }
+}
---
+++ Models/Prioridades.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ticket.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+
+        [Required(ErrorMessage = "El codigo de la prioridad es obligatorio")]
+        public string? Codigo { get; set; }
+    }
+}
