178f6af3e945ab28785611e9ced8a30872b59bbc
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2023-09-29 15:48:27 -0400

    Creando el modelo

---
+++ Models/Tickets.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ticket.Models
+{
+    public class Tickets
+    {
+        [Key]
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "El campo fecha es obligatorio")]
+        public DateOnly Fecha { get; set; }
+
+        [Required(ErrorMessage = "El campo ClienteId obligatorio")]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "El sistemaId es obligatorio")]
+        public int SistemaId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public string? SolicitadoPor { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public string? Asunto { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public string? Descripcion { get; set; }
+    }
+}
