b822388f198fd5ed86b86641023cd0e4075007e7
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2023-09-29 17:02:24 -0400

    Agregando TickestBLL, ClientesBLL, TicketsBLL y las consultas

---
+++ BLL/PrioridadesBLL.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Ticket.DAL;
+using Ticket.Models;
+
+namespace Ticket.BLL
+{
+    public class PrioridadesBLL
+    {
+        private Context _context;
+        public PrioridadesBLL(Context context)
+        {
+            _context = context;
+        }
+        public bool Existe(int PrioridadId)
+        {
+            return _context.Prioridades.Any(o => o.PrioridadId == PrioridadId);
+        }
+        private bool Insertar(Prioridades Prioridades)
+        {
+            _context.Prioridades.Add(Prioridades);
+            return _context.SaveChanges() > 0;
+        }
+        private bool Modificar(Prioridades Prioridades)
+        {
+            var existe = _context.Prioridades.Find(Prioridades.PrioridadId);
+            if (existe != null)
+            {
+                _context.Entry(existe).CurrentValues.SetValues(Prioridades);
+                return _context.SaveChanges() > 0;
+            }
+
+            return false;
+        }
+        public bool Guardar(Prioridades Prioridades)
+        {
+            if (!Existe(Prioridades.PrioridadId))
+                return this.Insertar(Prioridades);
+            else
+                return this.Modificar(Prioridades);
+        }
+        public bool Eliminar(int PrioridadesId)
+        {
+            var eliminado = _context.Prioridades.Where(o => o.PrioridadId == PrioridadesId).SingleOrDefault();
+
+            if (eliminado != null)
+            {
+                _context.Entry(eliminado).State = EntityState.Deleted;
+                return _context.SaveChanges() > 0;
+            }
+            return false;
+        }
+        public Prioridades? Buscar(int PrioridadId)
+        {
+            return _context.Prioridades.Where(o => o.PrioridadId == PrioridadId).AsNoTracking().SingleOrDefault();
+        }
+        public List<Prioridades> GetList(Expression<Func<Prioridades, bool>> criterio)
+        {
+            return _context.Prioridades.AsNoTracking().Where(criterio).ToList();
+        }
+    }
+}
---
+++ BLL/TicketsBLL.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Ticket.DAL;
+using Ticket.Models;
+
+namespace Ticket.BLL
+{
+    public class TicketsBLL
+    {
+        private Context _context;
+        public TicketsBLL(Context context)
+        {
+            _context = context;
+        }
+        public bool Existe(int TicketId)
+        {
+            return _context.Tickets.Any(o => o.TicketId == TicketId);
+        }
+        private bool Insertar(Tickets Tickets)
+        {
+            _context.Tickets.Add(Tickets);
+            return _context.SaveChanges() > 0;
+        }
+        private bool Modificar(Tickets Tickets)
+        {
+            var existe = _context.Tickets.Find(Tickets.TicketId);
+            if (existe != null)
+            {
+                _context.Entry(existe).CurrentValues.SetValues(Tickets);
+                return _context.SaveChanges() > 0;
+            }
+            return false;
+        }
+        public bool Guardar(Tickets Tickets)
+        {
+            if (!Existe(Tickets.TicketId))
+                return this.Insertar(Tickets);
+            else
+                return this.Modificar(Tickets);
+        }
+        public bool Eliminar(int TicketId)
+        {
+            var eliminado = _context.Tickets.Where(o => o.TicketId == TicketId).SingleOrDefault();
+
+            if (eliminado != null)
+            {
+                _context.Entry(eliminado).State = EntityState.Deleted;
+                return _context.SaveChanges() > 0;
+            }
+            return false;
+        }
+        public Tickets? Buscar(int TicketId)
+        {
+            return _context.Tickets.Where(o => o.TicketId == TicketId).AsNoTracking().SingleOrDefault();
+        }
+        public List<Tickets> GetList(Expression<Func<Tickets, bool>> criterio)
+        {
+            return _context.Tickets.AsNoTracking().Where(criterio).ToList();
+        }
+        public bool VerificarExistencia(Tickets Ticket)
+        {
+            var TicketIgual = _context.Tickets.Any(o => o.ClienteId == Ticket.ClienteId);
+
+            if (TicketIgual)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
